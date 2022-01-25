619409e2df98d939d31ce9867f4a235a66456258
toddt <toddteis@gmail.com>
2022-01-26 07:04:51 +1000

    Added functions add(),subtract(),multiple() & divide and tested with numbers and string operator. not working with string numbers.

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
 
 
+
 function operate(num1, oper, num2) {
-    if(oper == '+') {add(num1, num2)};
-    if(oper == '-') {subtract(num1, num2)};
-    if(oper == '*') {multiple(num1, num2)};
-    if(oper == '/') {divide(num1, num2)};    
+    let result;
+    if (oper == '+') {
+        result = add(num1, num2);
+    } else if (oper == '-') {
+        result = subtract(num1, num2);
+    } else if (oper == '*') {
+        result = multiple(num1, num2)
+    } else if (oper == '/') {
+        result = divide(num1, num2);
+    } else {
+        result = "Not a vaid operator."
+    }  
+    return result;
+}
+
+function add(num1, num2) {
+    return (num1 + num2);
+}
+
+function subtract(num1, num2) {
+    return (num1 - num2);
+}
+
+function multiple(num1, num2) {
+    return (num1 * num2);
+}
+
+function divide(num1, num2) {
+    return (num1 / num2);
 }
