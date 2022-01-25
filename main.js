e61050e0e86413b782cbca9e8973ef8f53a63cc2
toddt <toddteis@gmail.com>
2022-01-26 06:00:26 +1000

    Added operate() with logic to call add, subtract, multiple and divide functions.

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+function operate(num1, oper, num2) {
+    if(oper == '+') {add(num1, num2)};
+    if(oper == '-') {subtract(num1, num2)};
+    if(oper == '*') {multiple(num1, num2)};
+    if(oper == '/') {divide(num1, num2)};    
+}
