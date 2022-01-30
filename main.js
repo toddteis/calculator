d332588fe3ade55c652d4f22b7a9ac48a83bfdc0
toddt <toddteis@gmail.com>
2022-01-30 11:56:21 +1000

    Refactored event listener creation.

--- main.js
+++ main.js
@@ -1,22 +1,43 @@
 const displayMainDefault = 0;
 const displayTopDefault = [1, '+', 2];
 
-const buttons = document.querySelectorAll('input.btn-square');
+const buttonsNumber = document.querySelectorAll('input.number');
+const buttonsOperator = document.querySelectorAll('input.operator');
+const buttonClear = document.querySelector('input.clear');
+const buttonDelete = document.querySelector('input.delete');
+const buttonEqual = document.querySelector('input.equals');
+const buttonDecimal = document.querySelector('input.decimal');
 const displayMainScreen = document.querySelector('.display-main')
 const displayTopScreen = document.querySelector('.display-top')
 
 let displayTopInput = displayTopDefault;
 let displayMainInput = displayMainDefault;
 
-console.log(displayMainInput);
 
-buttons.forEach((button) => {
+buttonsNumber.forEach((button) => {
     button.addEventListener('click', () => {
         controller(button.id)
     });
 });
 
+buttonsOperator.forEach((button) => {
+    button.addEventListener('click', () => {
+        controller(button.id)
+    });
+});
+
+buttonClear.addEventListener('click', () => { controller(buttonClear.id); })
+buttonDelete.addEventListener('click', () => { controller(buttonDelete.id); })
+buttonEqual.addEventListener('click', () => { controller(buttonEqual.id); })
+buttonDecimal.addEventListener('click', () => { controller(buttonDecimal.id); })
+
 function controller(para) {
+
+    // if (Number.isFinite(para)) {
+    //     console.log(`from inside the if statement: ${para}`)
+    // } else {
+    //     console.log(`from inside the else statement: ${para}`)
+    // }
     displayMainScreen.textContent = para;
     displayTopScreen.textContent = para;
 }
