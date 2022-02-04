120e53c183b5304c195877d25933c8985c1cefc6
toddt <toddteis@gmail.com>
2022-02-04 16:08:55 +1000

    Added operate(), add(), subtract(), multiple() & divide() & working logic.

--- main.js
+++ main.js
@@ -143,6 +143,38 @@ function equalsController() {
     console.log("from within equalsController")
 }
 
+function operate(num1, oper, num2) {
+    let result;
+    if (oper == '+') {
+        result = add(num1, num2);
+    } else if (oper == '-') {
+        result = subtract(num1, num2);
+    } else if (oper == '*') {
+        result = multiple(num1, num2)
+    } else if (oper == '/') {
+        result = divide(num1, num2);
+    } else {
+        result = "Not a vaid operator."
+    }  
+    return result;
+}
+
+function add(num1, num2) {
+    return (num1 + num2);
+}
+
+function subtract(num1, num2) {
+    return (num1 - num2);
+}
+
+function multiple(num1, num2) {
+    return (num1 * num2);
+}
+
+function divide(num1, num2) {
+    return (num1 / num2);
+}
+
 // OLD CODE
 
 
